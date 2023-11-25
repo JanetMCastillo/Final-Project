1cd6f8fed78f4ede1753cb69526e73532ac575b8
Janet Castillo <janetcastillo56@gmail.com>
2023-11-24 17:27:19 -0700

    api-change

--- front-end/src/utils/api.js
+++ front-end/src/utils/api.js
@@ -6,7 +6,7 @@ import formatReservationDate from "./format-reservation-date";
 import formatReservationTime from "./format-reservation-date";
 
 const API_BASE_URL =
- process.env.REACT_APP_API_BASE_URL || "https://final-backend-lhpj.onrender.com";
+ process.env.REACT_APP_API_BASE_URL || "http://localhost:5001";
 
 //const API_BASE_URL = "https://final-backend-lhpj.onrender.com";
 
